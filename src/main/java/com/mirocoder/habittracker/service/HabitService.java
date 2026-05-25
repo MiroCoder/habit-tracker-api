275291d85527180cf7ea7a78ab24a556f86393e2
Miroslav <138483137+MiroCoder@users.noreply.github.com>
2026-05-25 20:48:13 +0200

    Use database for habit actions

--- src/main/java/com/mirocoder/habittracker/service/HabitService.java
+++ src/main/java/com/mirocoder/habittracker/service/HabitService.java
@@ -1,32 +1,22 @@
 package com.mirocoder.habittracker.service;
 
+import com.mirocoder.habittracker.dto.HabitRequest;
 import com.mirocoder.habittracker.model.Habit;
 import com.mirocoder.habittracker.model.HabitStats;
-import org.springframework.stereotype.Service;
-import com.mirocoder.habittracker.dto.HabitRequest;
 import com.mirocoder.habittracker.repository.HabitRepository;
-import com.mirocoder.habittracker.dto.HabitRequest;
-import java.util.ArrayList;
+import org.springframework.stereotype.Service;
+
 import java.util.List;
 
 @Service
 public class HabitService {
 
-    private final List<Habit> habits = new ArrayList<>();
     private final HabitRepository habitRepository;
-    private long nextId = 4;
 
     public HabitService(HabitRepository habitRepository) {
         this.habitRepository = habitRepository;
-
-        habits.add(new Habit(1L,"Code", true, Habit.Priority.High));
-        habits.add(new Habit(2L,"German", false, Habit.Priority.Medium));
-        habits.add(new Habit(3L,"Stretch", false, Habit.Priority.Low));
     }
 
-//    public List<Habit> getAllHabits() {
-//        return habits;
-//    }
     public List<Habit> getAllHabits() {
         return habitRepository.findAll();
     }
@@ -43,32 +33,19 @@ public class HabitService {
         return counter;
     }
 
-    public static Habit findHabit(List<Habit> habits, String name) {
-        for (Habit h : habits) {
-            if (h.getName().equalsIgnoreCase(name)) {
-                return h;
-            }
-        }
-
-        return null;
-    }
-
     public Habit findById(long id) {
-        for (Habit h : habits) {
-            if(h.getId() == id){
-                return h;
-            }
-        }
-        return null;
+        return habitRepository.findById(id);
     }
 
     public Habit markCompleted(long id) {
-        Habit habit = findById(id);
+        Habit habit = habitRepository.findById(id);
 
-        if (habit == null) { return null;}
+        if (habit == null) {
+            return null;
+        }
 
         habit.setCompleted(true);
-        return habit;
+        return habitRepository.update(habit);
     }
 
     public static String dayType(int habitsAmount, int habitsDone) {
@@ -91,18 +68,6 @@ public class HabitService {
         return (completedHabits * 100.0) / totalHabits;
     }
 
-//    public Habit addHabit(HabitRequest request) {
-//        Habit habit = new Habit(
-//                nextId++,
-//                request.getName(),
-//                request.isCompleted(),
-//                request.getPriority()
-//        );
-//
-//        habits.add(habit);
-//        return habit;
-//    }
-
     public Habit addHabit(HabitRequest request) {
         Habit habit = new Habit(
                 0,
@@ -114,15 +79,6 @@ public class HabitService {
         return habitRepository.save(habit);
     }
 
-//    public HabitStats getStats() {
-//        int completed = calculateCompletion(habits);
-//        int total = habits.size();
-//        int notCompleted = total - completed;
-//        double percent = dayPercent(total, completed);
-//        String dayType = dayType(total, completed);
-//
-//        return new HabitStats(total,completed, notCompleted, percent, dayType);
-//    }
     public HabitStats getStats() {
         List<Habit> habits = habitRepository.findAll();
 
@@ -136,9 +92,9 @@ public class HabitService {
     }
 
     public Habit updateHabit(long id, Habit updatedHabit) {
-        Habit habit = findById(id);
+        Habit habit = habitRepository.findById(id);
 
-        if(habit == null) {
+        if (habit == null) {
             return null;
         }
 
@@ -146,36 +102,18 @@ public class HabitService {
         habit.setCompleted(updatedHabit.isCompleted());
         habit.setPriority(updatedHabit.getPriority());
 
-        return habit;
+        return habitRepository.update(habit);
     }
 
     public boolean deleteHabit(long id) {
-        Habit habit = findById(id);
-
-        if(habit == null) {
-            return false;
-        }
-
-        habits.remove(habit);
-        return true;
+        return habitRepository.deleteById(id);
     }
 
     public Habit findByName(String name) {
-        for (Habit h: habits) {
-            if (h.getName().equals(name)){
-                return h;
-            }
-        }
-        return null;
+        return habitRepository.findByName(name);
     }
 
     public List<Habit> getHabitsByPriority(Habit.Priority priority) {
-        List<Habit> result = new ArrayList<>();
-        for (Habit h: habits) {
-            if (h.getPriority().equals(priority)){
-                result.add(h);
-            }
-        }
-        return result;
+        return habitRepository.findByPriority(priority);
     }
 }
