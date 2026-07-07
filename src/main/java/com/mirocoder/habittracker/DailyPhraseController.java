52487fd952c44ad44e25a7e01e0b4f263fcf74b8
Miroslav <138483137+MiroCoder@users.noreply.github.com>
2026-07-08 01:20:57 +0200

    Add daily phrase creation endpoint

---
+++ src/main/java/com/mirocoder/habittracker/DailyPhraseController.java
@@ -0,0 +1,26 @@
+package com.mirocoder.habittracker;
+
+import com.mirocoder.habittracker.dto.DailyPhraseRequest;
+import com.mirocoder.habittracker.service.DailyPhraseService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class DailyPhraseController {
+
+    private final DailyPhraseService dailyPhraseService;
+
+    public DailyPhraseController(DailyPhraseService dailyPhraseService){
+        this.dailyPhraseService = dailyPhraseService;
+    }
+
+    @PostMapping("/daily-phrases")
+    public ResponseEntity<Void> addDailyPhrase(
+            @Valid @RequestBody DailyPhraseRequest request
+    ) {
+        dailyPhraseService.addDailyPhrase(request);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+}
